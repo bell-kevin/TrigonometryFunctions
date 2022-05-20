c2df98a3e1b0c9e2656b94fba5934eb9d6a994ec
Kevin Bell <bellKevin@pm.me>
2022-05-20 01:35:49 +0000

    Update TrigonometryFunctions.java

--- trigonometryFunctions/src/trigonometryfunctions/TrigonometryFunctions.java
+++ trigonometryFunctions/src/trigonometryfunctions/TrigonometryFunctions.java
@@ -1,23 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package trigonometryfunctions;
 
 import java.util.Scanner;
 
-/**
- *
- * @author 4800590195
- */
 public class TrigonometryFunctions {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
         System.out.print("Math Trigonometric Functions by Kevin Bell \n");
         Scanner computerKeyboardInput = new Scanner(System.in);
         System.out.print("Please enter an angle in degrees: ");
@@ -27,8 +14,8 @@ public class TrigonometryFunctions {
         sine = Math.sin(angleInRadians);
         cosine = Math.cos(angleInRadians);
         tangent = Math.tan(angleInRadians);
-        System.out.printf("Sine: %,.4f\n", sine);
-        System.out.printf("Cosine: %,.4f\n", cosine);
-        System.out.printf("Tangent: %,.4f\n", tangent);
+        System.out.printf("%9s%,.4f\n", "Sine: ", sine);
+        System.out.printf("%9s%,.4f\n", "Cosine: ", cosine);
+        System.out.printf("%9s%,.4f\n", "Tangent: ", tangent);
     }
 }
